7bef5a8b8813a71785865fc8bb43eb2b01285858
Daniel Liebeskind <daniel.liebeskind@gmail.com>
2023-05-15 19:11:09 -0700

    Makes updatePosition more dynamic

--- server/utils/droppedAssets/droppedAssetSettings/updatePosition.js
+++ server/utils/droppedAssets/droppedAssetSettings/updatePosition.js
@@ -1,11 +1,14 @@
-import { getDroppedAssetDetails } from "../getDetails.js";
+import { getDroppedAssetDetails, getEmbeddedAssetDetails } from "../getDetails.js";
 import error from "../../errors.js";
 
 export const updatePosition = async (req, res) => {
   try {
     const { position } = req.body; // {x: number, y: number}
+    // If moving the asset with the iFrame embedded, won't have instanceId in the query.
+    // Can move another asset other than the one iFrame / webhook embedded inside of by passing instanceId
+    const { instanceId } = req.query;
     if (!position || !position.x || !position.y) throw "No position passed";
-    const droppedAsset = await getDroppedAssetDetails(req);
+    const droppedAsset = instanceId ? await getDroppedAssetDetails(req) : await getEmbeddedAssetDetails(req);
     if (!droppedAsset) throw "No dropped asset found";
     if (droppedAsset.error) throw droppedAsset.error;
     await droppedAsset.updatePosition(position.x, position.y);
