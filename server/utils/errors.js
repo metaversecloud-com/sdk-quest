6ceb59274065aac782c5f29b020e326b106d95c6
Daniel Liebeskind <daniel.liebeskind@gmail.com>
2023-05-24 21:18:20 -0700

    Improves error handling

--- server/utils/errors.js
+++ server/utils/errors.js
@@ -1,5 +1,5 @@
 export default (message, e, res) => {
-  // if (e && e.data && e.data.errors) console.log(`Error: ${message}`, e?.data?.errors);
+  if (e && e.data && e.data.errors) console.log(`Error Obj: ${message}`, e?.data?.errors);
   // else
   console.log(`Error: ${message}`, e);
   if (res) res.status(500).send({ error: e, success: false });
