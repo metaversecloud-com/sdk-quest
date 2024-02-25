fb01093f6fd9da3396ba543a98e69afd997c6db6
Lina <linabell@mail.com>
2024-02-26 09:38:28 +1300

    update path to package.json

--- server/utils/getVersion.js
+++ server/utils/getVersion.js
@@ -1,8 +1,12 @@
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 export const getVersion = () => {
   try {
-    const packageJsonContent = fs.readFileSync("./package.json", "utf8");
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const packageJsonContent = fs.readFileSync(path.resolve(__dirname, "../../package.json"));
     const packageJson = JSON.parse(packageJsonContent);
     const version = packageJson.version;
     return version;
