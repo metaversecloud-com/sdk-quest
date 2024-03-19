6f007948064e730065c063b26246550e82d77413
Lina <linabell@mail.com>
2024-03-19 10:46:26 -0700

    update world data object structure and leaderboard

--- server/utils/index.js
+++ server/utils/index.js
@@ -4,6 +4,7 @@ export * from "./world/index.js";
 export * from "./errorHandler.js";
 export * from "./getBaseURL.js";
 export * from "./getDefaultKeyAssetImage.js";
+export * from "./getDifferenceInDays.js";
 export * from "./getLongestStreak.js";
 export * from "./getRandomCoordinates.js";
 export * from "./topiaInit.js";
